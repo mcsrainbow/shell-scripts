7a348eb05250053a9a8dae45a062bc1140d9fe48
mcsrainbow <guosuiyu@gmail.com>
2015-03-12 19:19:32 +0800

    Update dns_ops.sh

--- scripts/dns_ops.sh
+++ scripts/dns_ops.sh
@@ -9,8 +9,7 @@ domain=heylinux.com
 private_file=${base_dir}/Kheylinux.com.+157+59510.private
 dnsaddfile=${base_dir}/dnsadd
 
-function check_root()
-{
+function check_root(){
   if [ $EUID -ne 0 ]; then
     echo "This script must be run as root" 1>&2
     exit 1
