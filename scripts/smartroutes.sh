57100a7bc14244029be7a75dd61a9eb7bd869962
mcsrainbow <guosuiyu@gmail.com>
2015-04-01 20:14:29 +0800

    Update smartroutes.sh to download with silent mode

--- scripts/smartroutes.sh
+++ scripts/smartroutes.sh
@@ -40,8 +40,13 @@ function check_size(){
 
 function update_data(){
   echo -n "Downloading the latest apnic_data: ${apnic_data}..."
+
   wget ${apnic_data_url} -q -O ${apnic_data}
-  echo " Done"
+  if [ $? -eq 0 ]; then
+    echo " Done"
+  else
+    echo " Failed"
+  fi
 }
 
 function check_status(){
