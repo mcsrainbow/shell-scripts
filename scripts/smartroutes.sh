da63783924eb3944fa77377f7a02732626fd47b9
mcsrainbow <guosuiyu@gmail.com>
2015-09-09 17:21:42 +0800

    Reformatted the script smartroutes.sh as Google-Style

--- scripts/smartroutes.sh
+++ scripts/smartroutes.sh
@@ -11,7 +11,7 @@ subnet_exceptions=(
 ) # subnets not in apnic_data, just some examples, change/remove them
 
 function check_root(){
-  if [ $EUID -ne 0 ]; then
+  if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root" 1>&2
     exit 1
   fi
@@ -19,12 +19,12 @@ function check_root(){
 
 function print_help(){
   echo "Usage:"
-  echo "${0} {on|off}"
-  echo "${0} [force|exception] {on|off}"
+  echo "  ${0} {on|off}"
+  echo "  ${0} [force|exception] {on|off}"
 }
 
 function check_data(){
-  if [ ! -f ${apnic_data} ]; then
+  if [[ ! -f ${apnic_data} ]]; then
     update_data
   fi
 }
@@ -33,7 +33,7 @@ function check_size(){
   apnic_data_size=$(curl --head -s ${apnic_data_url} |grep Content-Length |awk '{print $2}' |col -b)
   apnic_data_size_local=$(ls -l ${apnic_data} |awk '{print $5}')
 
-  if [ "${apnic_data_size}" != "${apnic_data_size_local}" ]; then
+  if [[ "${apnic_data_size}" != "${apnic_data_size_local}" ]]; then
     update_data
   fi
 }
@@ -42,24 +42,24 @@ function update_data(){
   echo "Downloading the latest APNIC data as ${apnic_data}..."
 
   curl --progress-bar -o ${apnic_data} ${apnic_data_url}
-  if [ $? -ne 0 ]; then
+  if [[ $? -ne 0 ]]; then
     exit 1
   fi
 }
 
 function check_status(){
   netstat -rn | grep -Eq "^${subnet_sign}"
-  if [ $? -ne 0 ]; then
+  if [[ $? -ne 0 ]]; then
     echo "SmartRoutes is OFF"
   else
     echo "SmartRoutes is ON"
   fi
 
-  if [ ! -z "${subnet_exceptions[0]}" ]; then
+  if [[ ! -z "${subnet_exceptions[0]}" ]]; then
     subnet_exception_formatted=$(format_subnet ${subnet_exceptions[0]})
 
     netstat -rn | grep -Eq "^${subnet_exception_formatted}"
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
       echo "SmartRoutes Exception is OFF"
     else
       echo "SmartRoutes Exception is ON"
@@ -73,8 +73,7 @@ function add_routes(){
 
   all_subs=$(grep CN ${apnic_data} | grep ipv4 | awk -F '|' '{print $4"/"$5}')
   echo -n "Adding the routes..."
-  for subnet in ${all_subs}
-  do
+  for subnet in ${all_subs}; do
     subneti=$(echo ${subnet} | cut -d/ -f1)
     rawnetm=$(echo ${subnet} | cut -d/ -f2)
     subnetm=$(awk -v c=${rawnetm} 'function log2(x){if(x<2)return(pow);pow--;return(log2(x/2))}BEGIN{pow=32;print log2(c)}')
@@ -86,8 +85,7 @@ function add_routes(){
 function del_routes(){
   all_subs=$(grep CN ${apnic_data} | grep ipv4 | awk -F '|' '{print $4"/"$5}')
   echo -n "Deleting the routes..."
-  for subnet in ${all_subs}
-  do
+  for subnet in ${all_subs}; do
     subneti=$(echo ${subnet} | cut -d/ -f1)
     rawnetm=$(echo ${subnet} | cut -d/ -f2)
     subnetm=$(awk -v c=${rawnetm} 'function log2(x){if(x<2)return(pow);pow--;return(log2(x/2))}BEGIN{pow=32;print log2(c)}')
@@ -98,7 +96,7 @@ function del_routes(){
 
 function run_smartroutes(){
   netstat -rn | grep -Eq "^${subnet_sign}"
-  if [ $? -ne 0 ]; then
+  if [[ $? -ne 0 ]]; then
     add_routes
   else
     echo "SmartRoutes is already ON"
@@ -107,7 +105,7 @@ function run_smartroutes(){
 
 function del_smartroutes(){
   netstat -rn | grep -Eq "^${subnet_sign}"
-  if [ $? -eq 0 ]; then
+  if [[ $? -eq 0 ]]; then
     del_routes
   else
     echo "SmartRoutes is already OFF"
@@ -121,29 +119,28 @@ function format_subnet(){
   d=$(echo $1 |cut -d/ -f1 |cut -d. -f4)
   m=$(echo $1 |cut -d/ -f2)
 
-  if [ $m -gt 24 ]; then
+  if [[ $m -gt 24 ]]; then
     echo "$a.$b.$c.$d/$m"
-  elif [ $m -le 24 ] && [ $m -gt 16 ]; then
+  elif [[ $m -le 24 ]] && [[ $m -gt 16 ]]; then
     echo "$a.$b.$c/$m"
-  elif [ $m -le 16 ] && [ $m -gt 8 ]; then
+  elif [[ $m -le 16 ]] && [[ $m -gt 8 ]]; then
     echo "$a.$b/$m"
-  elif [ $m -eq 8 ]; then
+  elif [[ $m -eq 8 ]]; then
     echo "$a/$m"
   fi
 }
 
 function add_exception(){
-  if [ ! -z "${subnet_exceptions[0]}" ]; then
+  if [[ ! -z "${subnet_exceptions[0]}" ]]; then
     subnet_exception_formatted=$(format_subnet ${subnet_exceptions[0]})
 
     netstat -rn | grep -Eq "^${subnet_exception_formatted}"
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
       oldgw=$(netstat -nr | grep '^default' | grep -v 'ppp' | sed 's/default *\([0-9\.]*\) .*/\1/' | grep -Ev '^$')
       dscacheutil -flushcache
 
       echo -n "Adding the routes..."
-      for subnet_exception in ${subnet_exceptions[@]}
-      do
+      for subnet_exception in ${subnet_exceptions[@]}; do
         route add ${subnet_exception} "${oldgw}" > /dev/null
       done
       echo " Done"
@@ -154,16 +151,15 @@ function add_exception(){
 }
 
 function del_exception(){
-  if [ ! -z "${subnet_exceptions[0]}" ]; then
+  if [[ ! -z "${subnet_exceptions[0]}" ]]; then
     subnet_exception_formatted=$(format_subnet ${subnet_exceptions[0]})
 
     netstat -rn | grep -Eq "^${subnet_exception_formatted}"
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
       echo "SmartRoutes Exception is already OFF"
     else
       echo -n "Deleting the routes..."
-      for subnet_exception in ${subnet_exceptions[@]}
-      do
+      for subnet_exception in ${subnet_exceptions[@]}; do
         route delete ${subnet_exception} > /dev/null
       done
     echo " Done"
