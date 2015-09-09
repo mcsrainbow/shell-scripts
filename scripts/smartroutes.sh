f7bd5fd3dd7108d51e9ebb76aad2b152646f057f
mcsrainbow <guosuiyu@gmail.com>
2015-09-09 17:37:10 +0800

    Updated smartroutes.sh

--- scripts/smartroutes.sh
+++ scripts/smartroutes.sh
@@ -68,14 +68,14 @@ function check_status(){
 }
 
 function add_routes(){
-  oldgw=$(netstat -nr | grep '^default' | grep -v 'ppp' | sed 's/default *\([0-9\.]*\) .*/\1/' | grep -Ev '^$')
+  oldgw=$(netstat -nr |grep '^default' |grep -v 'ppp' |sed 's/default *\([0-9\.]*\) .*/\1/' |grep -Ev '^$')
   dscacheutil -flushcache
 
-  all_subs=$(grep CN ${apnic_data} | grep ipv4 | awk -F '|' '{print $4"/"$5}')
+  all_subs=$(grep CN ${apnic_data} |grep ipv4 |awk -F '|' '{print $4"/"$5}')
   echo -n "Adding the routes..."
   for subnet in ${all_subs}; do
-    subneti=$(echo ${subnet} | cut -d/ -f1)
-    rawnetm=$(echo ${subnet} | cut -d/ -f2)
+    subneti=$(echo ${subnet} |cut -d/ -f1)
+    rawnetm=$(echo ${subnet} |cut -d/ -f2)
     subnetm=$(awk -v c=${rawnetm} 'function log2(x){if(x<2)return(pow);pow--;return(log2(x/2))}BEGIN{pow=32;print log2(c)}')
     route add ${subneti}/${subnetm} "${oldgw}" > /dev/null
   done
@@ -83,11 +83,11 @@ function add_routes(){
 }
 
 function del_routes(){
-  all_subs=$(grep CN ${apnic_data} | grep ipv4 | awk -F '|' '{print $4"/"$5}')
+  all_subs=$(grep CN ${apnic_data} |grep ipv4 |awk -F '|' '{print $4"/"$5}')
   echo -n "Deleting the routes..."
   for subnet in ${all_subs}; do
-    subneti=$(echo ${subnet} | cut -d/ -f1)
-    rawnetm=$(echo ${subnet} | cut -d/ -f2)
+    subneti=$(echo ${subnet} |cut -d/ -f1)
+    rawnetm=$(echo ${subnet} |cut -d/ -f2)
     subnetm=$(awk -v c=${rawnetm} 'function log2(x){if(x<2)return(pow);pow--;return(log2(x/2))}BEGIN{pow=32;print log2(c)}')
     route delete ${subneti}/${subnetm} > /dev/null
   done
@@ -136,7 +136,7 @@ function add_exception(){
 
     netstat -rn | grep -Eq "^${subnet_exception_formatted}"
     if [[ $? -ne 0 ]]; then
-      oldgw=$(netstat -nr | grep '^default' | grep -v 'ppp' | sed 's/default *\([0-9\.]*\) .*/\1/' | grep -Ev '^$')
+      oldgw=$(netstat -nr |grep '^default' |grep -v 'ppp' |sed 's/default *\([0-9\.]*\) .*/\1/' |grep -Ev '^$')
       dscacheutil -flushcache
 
       echo -n "Adding the routes..."
