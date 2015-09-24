10975a5b429b66162296747a4ccb480fb786f924
Dong Guo <guosuiyu@gmail.com>
2015-09-24 21:53:04 +0800

    Update dns_ops.sh

--- scripts/dns_ops.sh
+++ scripts/dns_ops.sh
@@ -10,7 +10,7 @@ private_file="${base_dir}/Kheylinux.com.+157+59510.private"
 dnsaddfile="${base_dir}/dnsadd"
 
 function check_root(){
-  if [ $EUID -ne 0 ]; then
+  if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root" 1>&2
     exit 1
   fi
@@ -30,7 +30,7 @@ function print_help(){
 
 function check_servername(){
   echo $servername | grep -wq ${domain}
-  if [ $? -eq 0 ]; then
+  if [[ $? -eq 0 ]]; then
     hostname=$(echo $servername | cut -d. -f1)
     echo "'${servername}' is malformed. Servername should be just '${hostname}' without the '${domain}'"
     exit 1
@@ -39,7 +39,7 @@ function check_servername(){
 
 function check_fqdn(){
   echo $record_value | grep -q '\.'
-  if [ $? -ne 0 ]; then
+  if [[ $? -ne 0 ]]; then
     echo "'${record_value}' is malformed. Should be a FQDN"
     exit 1
   fi 
@@ -47,14 +47,14 @@ function check_fqdn(){
 
 function check_prereq(){
   # Check if the prerequisite is satisfied, such as duplicate and nonexistent
-  if [ $action == "add" ]; then
-    if [ $record_type == "PTR" ]; then
+  if [[ $action == "add" ]]; then
+    if [[ $record_type == "PTR" ]]; then
       echo "prereq nxrrset ${servername}.${domain} ${record_type} ${record_value}" >> ${dnsaddfile} 
     else
       echo "prereq nxdomain ${servername}.${domain}" >> ${dnsaddfile} 
     fi
   fi
-  if [ $action == "delete" ]; then
+  if [[ $action == "delete" ]]; then
     echo "prereq yxrrset ${servername}.${domain} ${record_type} ${record_value}" >> ${dnsaddfile} 
   fi
 }
@@ -69,12 +69,12 @@ function update_record(){
   echo "update $action ${servername}.${domain} 86400 ${record_type} ${record_value}"
   /usr/bin/nsupdate -k ${private_file} ${dnsaddfile} 
 
-  if [ $? -eq 0 ]; then
+  if [[ $? -eq 0 ]]; then
     echo "Successful"
   else
-    if [ $action == "add" ]; then
+    if [[ $action == "add" ]]; then
       echo "Failed because duplicate record"
-    elif [ $action == "delete" ]; then
+    elif [[ $action == "delete" ]]; then
       echo "Failed because nonexistent/protected record"
     fi
     exit $?
@@ -108,7 +108,7 @@ do
   esac
 done
 
-if [ -z "$record_type" ] || [ -z "$action" ] || [ -z "$servername" ] || [ -z "$record_value" ]; then
+if [[ -z "$record_type" ]] || [[ -z "$action" ]] || [[ -z "$servername" ]] || [[ -z "$record_value" ]]; then
   print_help
 else
   > ${dnsaddfile}
@@ -139,7 +139,7 @@ else
       b=$(echo $servername |cut -d. -f2 |grep -Ev '[a-z]|[A-Z]')
       c=$(echo $servername |cut -d. -f3 |grep -Ev '[a-z]|[A-Z]')
       d=$(echo $servername |cut -d. -f4 |grep -Ev '[a-z]|[A-Z]')
-      if [ -z "$a" ] || [ -z "$b" ] || [ -z "$c" ] || [ -z "$d" ]; then
+      if [[ -z "$a" ]] || [[ -z "$b" ]] || [[ -z "$c" ]] || [[ -z "$d" ]]; then
         echo "'${servername}' is malformed. Should be a IP address"
       else
         domain=$c.$b.$a.in-addr.arpa
