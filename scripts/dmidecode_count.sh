cd24366c9cce8ad2be080d2f7d613bc2de813013
Dong Guo <dong@drawbrid.ge>
2015-10-23 07:03:20 +0000

    Update dmidecode_count.sh

---
+++ scripts/dmidecode_count.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Output examples: idc1-server1, X8STi, 1x4096 kB, 2x4096 MB, 4xNo Module Installed
+
+hostname=$(hostname -s)
+motherboard=$(dmidecode |grep "Product Name" |awk -F ': ' '{print $2}' |uniq |xargs)
+ram_all=$(dmidecode |grep 'Memory Device' -A 5 |grep Size: |grep -v Range |awk -F ': ' '{print "x"$2}' |grep -v 'No Module Installed' |sort |uniq -c)
+result=""
+for i in ${ram_all}
+do 
+  if $(echo $i |grep -q 'x'); then
+    result="$result$i"
+  elif $(echo $i |grep -q 'B' ); then
+    result="$result $i, "
+  else
+    result="$result$i"
+  fi
+done
+
+slots_open=$(dmidecode |grep 'Memory Device' -A 5 |grep Size: |grep -v Range |awk -F ': ' '{print $2}' |grep -c 'No Module Installed')
+
+echo "${hostname}, ${motherboard}, ${result}${slots_open}xNo Module Installed"
