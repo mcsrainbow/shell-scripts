0e0ba351ac253648b1517a1dacc689f27a7e83e1
mcsrainbow <guosuiyu@gmail.com>
2017-03-10 16:04:04 +0800

    Add create_tunnel.sh

---
+++ scripts/create_tunnel.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Create a SSH tunnel to access an unreachable web URL via another reachable host
+# For Mac OS only
+
+tunnel_host=${1}
+dest_host_item=${2}
+
+ssh_user="trump"
+ssh_key="/Users/trump/.ssh/id_rsa"
+
+if [[ $# -ne 2 ]] && [[ "${1}" != "list" ]]; then
+  echo "Usage: ${0} tunnel_host dest_host_item"
+  echo "       ${0} trump.heylinux.com http://tiffany.heylinux.com/#/pretty/girl"
+  echo "       ${0} trump.heylinux.com https://ivanka.heylinux.com:8443/#/pretty/girl"
+  echo "       ${0} list"
+  exit 1
+elif [[ "${1}" == "list" ]]; then
+  echo "SSH Tunnel Processes:"
+  ps aux | grep '\-f \-N \-T \-L'
+  exit 0
+fi
+
+dest_host_protocol=$(echo ${dest_host_item} | cut -d: -f1)
+dest_host_name=$(echo ${dest_host_item} | cut -d: -f2 | cut -d/ -f3)
+dest_host_port=$(echo ${dest_host_item} | cut -d: -f3 | cut -d/ -f1)
+dest_host_url=$(echo ${dest_host_item} | cut -d: -f3 | cut -d/ -f2-)
+
+if [[ -z "${dest_host_port}" ]]; then
+  dest_host_port=80
+  dest_host_url=$(echo ${dest_host_item} | cut -d: -f2 | cut -d/ -f4-)
+fi
+
+# Use a different port on localhost for ports in range 1-1024
+if [[ "${dest_host_port}" -le 1024 ]]; then
+  local_host_port=$((${dest_host_port}+10000))
+else
+  local_host_port=${dest_host_port}
+fi
+
+ssh -i ${ssh_key} -l ${ssh_user} -f -N -T -L ${local_host_port}:${dest_host_name}:${dest_host_port} ${tunnel_host}
+
+echo "Opening ${dest_host_protocol}://localhost:${local_host_port}/${dest_host_url}"
+open ${dest_host_protocol}://localhost:${local_host_port}/${dest_host_url}
