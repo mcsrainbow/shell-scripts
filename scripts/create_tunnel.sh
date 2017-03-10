a58b1e9d656147a6bc415f4d4662f274e7330d39
mcsrainbow <guosuiyu@gmail.com>
2017-03-10 17:54:01 +0800

    Update create_tunnel.sh

--- scripts/create_tunnel.sh
+++ scripts/create_tunnel.sh
@@ -3,14 +3,14 @@
 # For Mac OS only
 
 tunnel_host=${1}
-dest_host_item=${2}
+dest_host_full_url=${2}
 
 ssh_user="trump"
 ssh_port=22
 ssh_key="/Users/trump/.ssh/id_rsa"
 
 if [[ $# -ne 2 ]] && [[ "${1}" != "list" ]]; then
-  echo "Usage: ${0} tunnel_host dest_host_item"
+  echo "Usage: ${0} tunnel_host dest_host_full_url"
   echo "       ${0} trump.heylinux.com http://tiffany.heylinux.com/#/pretty/girl"
   echo "       ${0} trump.heylinux.com https://ivanka.heylinux.com:8443/#/pretty/girl"
   echo "       ${0} list"
@@ -25,13 +25,13 @@ elif [[ "${1}" == "list" ]]; then
   exit 0
 fi
 
-dest_host_protocol=$(echo ${dest_host_item} | cut -d: -f1)
-dest_host_name=$(echo ${dest_host_item} | cut -d: -f2 | cut -d/ -f3)
-dest_host_port=$(echo ${dest_host_item} | cut -d: -f3 | cut -d/ -f1)
-dest_host_url=$(echo ${dest_host_item} | cut -d: -f3 | cut -d/ -f2-)
+dest_host_protocol=$(echo ${dest_host_full_url} | cut -d: -f1)
+dest_host_name=$(echo ${dest_host_full_url} | cut -d: -f2 | cut -d/ -f3)
+dest_host_port=$(echo ${dest_host_full_url} | cut -d: -f3 | cut -d/ -f1)
+dest_host_short_url=$(echo ${dest_host_full_url} | cut -d: -f3 | cut -d/ -f2-)
 
 if ! $(echo "${dest_host_protocol}" | grep -Ewq 'http|https'); then
-  echo "Need to specify the protocol: http:// or https://"
+  echo "Need to specify the protocol in dest_host_full_url: 'http://' or 'https://'"
   exit 1
 fi
 
@@ -41,7 +41,7 @@ if [[ -z "${dest_host_port}" ]]; then
   elif [[ ${dest_host_protocol} == "https" ]]; then
     dest_host_port=443
   fi
-  dest_host_url=$(echo ${dest_host_item} | cut -d: -f2 | cut -d/ -f4-)
+  dest_host_short_url=$(echo ${dest_host_full_url} | cut -d: -f2 | cut -d/ -f4-)
 fi
 
 # Use a different port on localhost for ports which less than 10,000
@@ -53,5 +53,5 @@ fi
 
 ssh -i ${ssh_key} -p ${ssh_port} -l ${ssh_user} -f -N -T -L ${local_host_port}:${dest_host_name}:${dest_host_port} ${tunnel_host}
 
-echo "Opening ${dest_host_protocol}://localhost:${local_host_port}/${dest_host_url}"
-open ${dest_host_protocol}://localhost:${local_host_port}/${dest_host_url}
+echo "Opening ${dest_host_protocol}://localhost:${local_host_port}/${dest_host_short_url}"
+open ${dest_host_protocol}://localhost:${local_host_port}/${dest_host_short_url}
