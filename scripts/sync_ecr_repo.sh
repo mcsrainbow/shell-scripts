e3b364b96576329718faca4bfd10f65bab7fed3b
Dong Guo | Damon <guosuiyu@gmail.com>
2021-05-19 14:52:19 +0800

    Update sync_ecr_repo.sh

--- scripts/sync_ecr_repo.sh
+++ scripts/sync_ecr_repo.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 aws_cli="/usr/bin/aws"
-image_prefix="dkr.ecr.us-west-1.amazonaws.com"
+aws_region="us-west-1"
+
+image_prefix="dkr.ecr.${aws_region}.amazonaws.com"
 image_list=(
 redis:latest
 openjdk:8-jdk
@@ -9,32 +11,34 @@ openjdk:8-jdk
 
 function check_params(){
   if [ $# -ne 2 ]; then
-    echo "Usage: ${0} src_aws_id dest_aws_id"
-    echo "       ${0} 111111111111 222222222222"
+    echo "Usage: ${0} src_aws_id dst_aws_id"
+    echo "       ${0} 233233233233 857857857857"
     exit 1
   else
     src_aws_id=${1}
-    dest_aws_id=${2}
+    dst_aws_id=${2}
+    
+    docker_password=$(${aws_cli} ecr get-login-password --region ${aws_region})
   fi
 }
 
 function pull_ecr_images(){
-  $(${aws_cli} ecr get-login --no-include-email --region us-west-1 --registry-ids $src_aws_id)
+  docker login --username AWS --password $docker_password ${src_aws_id}.${image_prefix}
   for image in ${image_list[@]}; do
     docker pull ${src_aws_id}.${image_prefix}/${image}
   done
 }
 
 function push_ecr_images(){
-  $(${aws_cli} ecr get-login --no-include-email --region us-west-1 --registry-ids $dest_aws_id)
+  docker login --username AWS --password $docker_password ${dst_aws_id}.${image_prefix}
   repo_list=$(${aws_cli} ecr describe-repositories --output text | awk '($1=="REPOSITORIES"){print $6}')
   for image in ${image_list[@]}; do
     repo_name=$(echo $image | cut -d: -f1)
     if ! $(echo ${repo_list} | grep -wq "${repo_name}"); then
       ${aws_cli} ecr create-repository --repository-name ${repo_name}
     fi
-      docker tag ${src_aws_id}.${image_prefix}/${image} ${dest_aws_id}.${image_prefix}/${image}
-      docker push ${dest_aws_id}.${image_prefix}/${image}
+      docker tag ${src_aws_id}.${image_prefix}/${image} ${dst_aws_id}.${image_prefix}/${image}
+      docker push ${dst_aws_id}.${image_prefix}/${image}
   done
 }
 
