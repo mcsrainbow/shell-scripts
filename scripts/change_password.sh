95fda6a709ef148357d0c48b528fa843813ae8cb
Dong Guo | Damon <guosuiyu@gmail.com>
2021-10-25 17:50:20 +0800

    Create change_password.sh

---
+++ scripts/change_password.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/expect
+set timeout 5
+set host [lindex $argv 0]
+set user [lindex $argv 1]
+set oldpass [lindex $argv 2]
+set newpass [lindex $argv 3]
+
+spawn ssh -tq ${user}@${host}
+#spawn ssh ${user}@${host}
+expect "ssword:"
+send "${oldpass}\r"
+expect "${user}@"
+send "passwd\r"
+expect "current"
+send "${oldpass}\r"
+expect {
+       "New password:" {
+        send "${newpass}\r"
+     expect {
+     "BAD PASSWORD" {
+     exit 1 }
+     "Password unchanged" {
+     exit 1 }
+     "Authentication token manipulation error" {
+     exit 1 }
+     "Retype new password:" {
+     send "${newpass}\r"
+     expect "${user}@"
+     send "exit\r"
+     exit 0 }
+     eof {
+     exit 0 }
+     }
+}
+      "wait longer" {
+     exit 1 }
+}
