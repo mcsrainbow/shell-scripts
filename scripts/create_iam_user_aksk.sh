2800aa676b46c3138dd856e8eb212b8dd059bc9d
Dong Guo | Damon <guosuiyu@gmail.com>
2022-08-09 14:13:44 +0800

    Update create_iam_user_aksk.sh

--- scripts/create_iam_user_aksk.sh
+++ scripts/create_iam_user_aksk.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Description: Create IAM User, Group, Policy and genreate AKSK
+# Author: Damon Guo
+# Content:
+# .
+# ├── create_iam_user_aksk.sh
+# ├── generated_aksk
+# │   ├── HeyApp.json
+# └── policy_data
+#     └── HeyAppServicePolicy.json
 
 user_group_policy=(
 HeyApp,HeyAppGroup,HeyAppServicePolicy
@@ -6,7 +15,7 @@ HeyApp,HeyAppGroup,HeyAppServicePolicy
 
 aws_account_id=857857857857
 aws_cmd="/usr/local/bin/aws"
-srv_path=heysrv
+srv_path="heysrv"
 all_users="$(${aws_cmd} iam list-users --path /${srv_path}/ | grep -w UserName | xargs)"
 all_groups="$(${aws_cmd} iam list-groups --path /${srv_path}/ | grep -w GroupName | xargs)"
 all_policies="$(${aws_cmd} iam list-policies --path /${srv_path}/ | grep -w PolicyName | xargs)"
