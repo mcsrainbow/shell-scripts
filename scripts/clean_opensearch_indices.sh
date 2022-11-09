69ec25e27607165f6dfa356453e3f8b1784a49f8
Dong Guo | Damon <guosuiyu@gmail.com>
2022-11-09 11:16:47 +0800

    Update clean_opensearch_indices.sh

--- scripts/clean_opensearch_indices.sh
+++ scripts/clean_opensearch_indices.sh
@@ -7,7 +7,7 @@ indices_items=$(curl -s -XGET -k -u ${user_pass} "${opensearch_url}/_cat/indices
 clean_date=$(date --date="30 days ago" +%Y%m%d)
 
 for index_item in ${indices_items}; do
-  index_item_date=$(echo ${index_item} | awk -F '-' '{print $NF}' | sed s/[.]//g | grep -v '[a-Z]') 
+  index_item_date=$(echo ${index_item} | awk -F '-' '{print $NF}' | sed s/[.]//g | grep -Ev '[a-z]|[A-Z]') 
   if [ ! -z "${index_item_date}" ] && [ ${index_item_date} -lt ${clean_date} ]; then
     echo "Deleting index: ${index_item}"
     curl -s -XDELETE -k -u ${user_pass} "${opensearch_url}/${index_item}"
